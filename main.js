dc123cf42f708847df2e18ec057848b5c426cd94
KylerBest <kyler.b@live.com>
2021-12-06 15:41:31 -0600

    Cards now start 'hidden' and are revealed once clicked.

--- main.js
+++ main.js
@@ -6,10 +6,17 @@ const board = document.querySelector("#gameBoard");
         fetch(`https://deckofcardsapi.com/api/deck/${json.deck_id}/draw/?count=52`)
         .then(res => res.json())
         .then(json => {
-            console.log(json)
             for(const item of json.cards){
-                let card = document.createElement("img")
-                card.src = item.image
+                let card = document.createElement("div")
+                let img = document.createElement("img")
+                img.src = item.image
+                let hider = document.createElement("div")
+                hider.id = "hider"
+                card.addEventListener("click", function(){
+                    hider.classList.add("show")
+                })
+                card.appendChild(img)
+                card.appendChild(hider)
                 board.appendChild(card)
             }
         })
