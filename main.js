0783702641c57b91c2a0965c8a5b0006c208f578
KylerBest <kyler.b@live.com>
2021-12-06 17:20:12 -0600

    Clicking on two different cards of the same value now removes the pair and increases a score counter.

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-const board = document.querySelector("#gameBoard");
+const board = document.querySelector("#gameBoard")
+let lastCard = null
+let score = 0
+let lives = 10;
 (function initialize(){
     fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
     .then(res => res.json())
@@ -6,14 +9,45 @@ const board = document.querySelector("#gameBoard");
         fetch(`https://deckofcardsapi.com/api/deck/${json.deck_id}/draw/?count=52`)
         .then(res => res.json())
         .then(json => {
+            console.log(json)
             for(const item of json.cards){
                 let card = document.createElement("div")
+                card.id = `${item.code[0]}`
+
                 let img = document.createElement("img")
                 img.src = item.image
+
                 let hider = document.createElement("div")
                 hider.id = "hider"
+
                 card.addEventListener("click", function(){
-                    hider.classList.add("show")
+                    hider.classList.add("remove")
+                    setTimeout(function(){
+                        if(lastCard != null){
+                            if(lastCard.id == card.id){
+                                card.firstChild.classList.add("remove")
+                                lastCard.firstChild.classList.add("remove")
+
+                                lastCard = null
+
+                                score++
+                                console.log(`Score: ${score}`)
+                            }else{
+                                card.lastChild.classList.remove("remove")
+                                lastCard.lastChild.classList.remove("remove")
+                                
+                                lastCard = null
+                                
+                                lives--
+                                if(lives < 1){
+                                    board.style.display = "none"
+                                }
+                                console.log(`Lives: ${lives}`)
+                            }
+                        }else{
+                            lastCard = card
+                        }
+                    }, 1000)
                 })
                 card.appendChild(img)
                 card.appendChild(hider)
